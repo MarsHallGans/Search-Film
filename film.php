fb9c1e1e7668fce6617a98a6e4b334958be4e74a
MarsHallGans <47811548+MarsHallGans@users.noreply.github.com>
2020-01-11 12:51:42 +0000

    Add files via upload

---
+++ film.php
@@ -0,0 +1,34 @@
+<?php
+
+    echo "#################################\n";
+    echo "#  Search Film & Link Donwload  #\n";
+    echo "#      Create By MarsHall       #\n";
+    echo "#################################\n";
+    echo "Judul Film : ";
+    $judul = trim(fgets(STDIN));
+    
+ 
+function http_request($url){
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Linux; Android 5.0.2; Redmi Note 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.96 Mobile Safari/537.36');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    $output = curl_exec($ch); 
+    curl_close($ch);      
+    return $output;
+}
+$kontol = http_request("https://ducky-database-movies-api.herokuapp.com/api/movies?key_token=jIPQfBjAULJBmxno43EMMlIJlLwIuIm07fYK3o3CTM9Q25SmL2iED6CBlgkirYE2&search=$judul");
+$kontol = json_decode($kontol, TRUE);
+
+$title = $kontol['data'][0]['movieInformation']['title'];
+$synopsis = $kontol['data'][0]['movieInformation']['synopsis'];
+$dateRelease = $kontol['data'][0]['movieInformation']['dateRelease'];
+$poster = $kontol['data'][0]['movieInformation']['poster'];
+
+
+echo "\n[+] Judul Film : $title\n\n";
+echo "[+] Synopsis : $synopsis\n\n";
+echo "[+] Tanggal Rilis : $dateRelease\n\n";
+echo "[+] Link Donwload : $poster\n\n";
+
+?>
